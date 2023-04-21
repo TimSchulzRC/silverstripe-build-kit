0bce4360898c212ca2a727d5f3e3da2fb684f065
Tim Schulz <tim.schulz.rc@gmail.com>
2023-04-21 10:33:03 +0200

    custom field test

--- client/src/boot/registerComponents.js
+++ client/src/boot/registerComponents.js
@@ -1,7 +1,8 @@
-import Injector from 'lib/Injector';
+import Injector from "lib/Injector";
+import CustomField from "../components/CustomField";
 
 export default () => {
   Injector.component.registerMany({
-    // List your React components here so Injector is aware of them
+    CustomField,
   });
 };
---
+++ src/CustomField.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace src;
+
+class CustomField extends FieldGroup {
+    protected $schemaDataType = FormField::SCHEMA_DATA_TYPE_CUSTOM;
+
+    protected $schemaComponent = 'TextDropdownField';
+
+    /**
+     * @var TextField
+     */
+    private $textField;
+
+    /**
+     * @var DropdownField
+     */
+    private $dropdownField;
+
+    public function __construct(
+        string $name,
+        ?string $title = null,
+        string $textRelation,
+        string $dropdownRelation,
+        array $dropdownSource
+    ) {
+        $fields = [
+            $this->textField = TextField::create($textRelation),
+            $this->dropdownField = DropdownField::create($dropdownRelation)
+                ->setSource($dropdownSource)
+        ];
+
+        $this->addExtraClass('text-dropdown-field');
+        parent::__construct($title, $fields);
+    }
+
+    public function getSchemaStateDefaults()
+    {
+        $state = parent::getSchemaStateDefaults();
+        $state['textField'] = $this->textField->getSchemaState();
+        $state['dropdownField'] = $this->dropdownField->getSchemaState();
+        return $state;
+    }
+}
