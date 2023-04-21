5499caf3d4b47e47cf81caf8a10a5ad128d0774e
Tim Schulz <tim.schulz.rc@gmail.com>
2023-04-21 11:50:34 +0200

    remove unused imports

--- client/src/components/CustomField.js
+++ client/src/components/CustomField.js
@@ -1,13 +1,5 @@
 import React, { useEffect, useState } from "react";
 import fieldHolder from "components/FieldHolder/FieldHolder";
-import {
-  DropdownItem,
-  DropdownMenu,
-  DropdownToggle,
-  Input,
-  InputGroup,
-  InputGroupButtonDropdown,
-} from "reactstrap";
 
 const CustomField = () => <div>Hello World</div>;
 
